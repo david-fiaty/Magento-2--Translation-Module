765f3ec831768b1f8fdda8aaf9017821dd33494c
david-fiaty <dfiaty@gmail.com>
2019-08-11 06:01:41 +0100

    Removed the core js dependency

--- view/adminhtml/requirejs-config.js
+++ view/adminhtml/requirejs-config.js
@@ -1,7 +1,6 @@
 var config = {
     map: {
         '*': {
-            corejs:  'Naxero_Translation/js/translation/core',
             filesjs:  'Naxero_Translation/js/translation/files',
             stringsjs:  'Naxero_Translation/js/translation/strings'
         }
