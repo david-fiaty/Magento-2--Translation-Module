916b5a52808bf369f8aaff5798275ac35f06c7e5
david-fiaty <dfiaty@gmail.com>
2019-09-07 04:28:36 +0100

    Disabled row highlighting for logs list

--- Model/Service/LogDataService.php
+++ Model/Service/LogDataService.php
@@ -120,7 +120,6 @@ class LogDataService
 
                 // Add to output
                 $this->output['table_data'][] = (object) $arr;
-                $this->output['error_data'][] = (object) $arr;
 
                 // Increment the file count
                 $fileCount++;
