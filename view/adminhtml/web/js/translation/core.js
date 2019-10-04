561097fdcecc545da6259823bd229a6e85da89c8
david-fiaty <dfiaty@gmail.com>
2019-10-04 08:30:49 +0100

    Optimised the js files structure

--- view/adminhtml/web/js/translation/core.js
+++ view/adminhtml/web/js/translation/core.js
@@ -15,10 +15,11 @@
 define(
     [
         'jquery',
+        'Naxero_Translation/js/translation/filters',
         'mage/translate',
         'mage/cookies'
     ],
-    function ($, __) {
+    function ($, filters, __) {
         'use strict';
 
         // Define constants
@@ -112,10 +113,10 @@ define(
                         self.setPaging(com, com.options.targetTable);
 
                         // Build options for the lists
-                        self.buildFilters(com, data);
+                        filters.build(com, data);
     
                         // Add the list events
-                        self.addFilterEvents(com);
+                        filters.addEvents(com);
 
                         // Handle invalid rows display
                         if (data.error_data) {
@@ -230,102 +231,6 @@ define(
                         self.showMessage(com, 'error', error);
                     }
                 });
-            },
-
-            buildFilters: function(com, data) {
-                if (com.filters && data.filter_data) {
-                    // Create the group filter
-                    this.createOptions(com, com.filters.group, data.filter_data.file_group);
-        
-                    // Create the type filter
-                    this.createOptions(com, com.filters.type, data.filter_data.file_type);
-        
-                    // Create the locale filter
-                    this.createOptions(com, com.filters.locale, data.filter_data.file_locale);
-        
-                    // Create the status filter
-                    this.createOptions(com, com.filters.status, data.filter_data.file_status);
-                }
-            },
-
-            createOptions: function(com, sel, arr) {
-                // Prepare the options
-                var output = [];
-                $.each(arr, function(key, value) {
-                    // Create the option
-                    var option = '<option value="' + value + '">' + value + '</option>';
-    
-                    // Add it to the output
-                    output.push(option);
-                });
-
-                // Append the options
-                com.cache._(sel).children('option:not(:first)').remove();
-                com.cache._(sel).append(output.join(''));
-            },
-
-            addFilterEvents: function(com) {
-                var self = this;
-
-                // Pager events
-                com.cache._('.' + PAGER_SELECTOR).off().on('change', function() {
-                    let selectedKey = $(this).find(':selected').val();
-                    self.setPaging(com, com.options.targetTable, selectedKey);
-                });
-
-                // Filters events
-                if (com.filters) {
-                    var self = this;
-
-                    // Prepare the fields
-                    var fields = [
-                        {selector: com.filters.group, field: 'file_group'},
-                        {selector: com.filters.type, field: 'file_type'},
-                        {selector: com.filters.locale, field: 'file_locale'}
-                    ];
-        
-                    // Assign the events
-                    $.each(fields, function(k, obj) {
-                        com.cache._(obj.selector).off().on('change', function() {
-                            let selectedKey = $(this).find(':selected').val();
-                            self.updateFilters(
-                                com,
-                                {field: obj.field, type: '=', value: selectedKey}
-                            );
-                        });
-                    });
-                }
-            },
-
-            updateFilters: function(com, newFilter) {
-                // Get the existing filters
-                var filters = $(com.options.targetTable).tabulator('getFilters'); 
-                var found = filters.find(function(element) {
-                    return element.field == newFilter.field;
-                });
-    
-                // Process the new filter
-                if (filters.length == 0 || typeof found === 'undefined') {
-                    filters.push(newFilter);
-                } else {
-                    for (var i = 0; i < filters.length; i++) {
-                        if (filters[i].field == newFilter.field) {
-                            if (newFilter.value === 'alltx') {
-                                filters.splice(i, 1);
-                            } 
-                            else if (newFilter.value !== 'alltx' && newFilter.field === filters[i].field) {
-                                filters[i] = newFilter;
-                            }
-                        } 
-                        else if (filters[i].field == newFilter.field && newFilter.value !== 'alltx') {
-                            filters.push(newFilter);
-                        }                    
-                    }
-                }
-    
-                // Clear filters and set the new one
-                com.cache._(com.options.targetTable).tabulator('clearFilter');
-                com.cache._(com.options.targetTable).tabulator('setFilter', filters);
             },
 
             updateEntityData: function(com, data) {
